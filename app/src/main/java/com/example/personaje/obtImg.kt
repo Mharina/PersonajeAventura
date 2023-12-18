fb4ce3ccc934e72a1e497d7dc7ed04fac7e11269
MrSixtorz <sixto723@gmail.com>
2023-12-18 19:43:57 +0100

    Funciona todo a falta de obtener una solución más óptima.

--- app/src/main/java/com/example/personaje/obtImg.kt
+++ app/src/main/java/com/example/personaje/obtImg.kt
@@ -1,54 +1,8 @@
 package com.example.personaje
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import com.example.personaje.obtImg
 
-class PersonajeMostrar : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_personaje_mostrar)
-
-        val pj = intent.getParcelableExtra<Personaje>("personaje")
-        //val img = intent.getStringExtra("imagen_id")
-
-        val nombre = findViewById<TextView>(R.id.textView5)
-        val raza = findViewById<TextView>(R.id.textView7)
-        val clase = findViewById<TextView>(R.id.textView9)
-        val estadoVital = findViewById<TextView>(R.id.textView11)
-        var foto: ImageView = findViewById(R.id.imageView2)
-
-        //foto.setImageResource(img.drawa)
-        nombre.text = "${pj?.getNombre()}"
-        raza.text = "${pj?.getRaza()}"
-        clase.text = "${pj?.getClase()}"
-        estadoVital.text = "${pj?.getEstadoVital()}"
-        var btnVolver: Button = findViewById(R.id.Volver1)
-        var btnComenzar: Button = findViewById(R.id.empezar)
-//        val recursoImg = "${pj?.getRaza()?.toLowerCase()}"+
-//                "${pj?.getClase()?.toLowerCase()}"+
-//                "${pj?.getRaza()?.toLowerCase()}"+".jpg"
-//        val idRecurso = resources.getIdentifier(recursoImg, "drawable", packageName)
-//        if (idRecurso!=0)
-//            foto.setImageResource(idRecurso)
-//        else
-//            foto.setImageResource(R.drawable.comodin)
-        obtenerImagen3(foto,pj?.getRaza().toString(),pj?.getClase().toString(),pj?.getEstadoVital().toString())
-
-
-        btnVolver.setOnClickListener {
-            val intent = Intent(this@PersonajeMostrar, MainActivity::class.java)
-            startActivity(intent)
-        }
-        btnComenzar.setOnClickListener {
-            val intent = Intent(this@PersonajeMostrar, Aventura::class.java)
-            startActivity(intent)
-        }
-    }
+class obtImg {
     fun obtenerImagen3(
         foto: ImageView,
         seleccionRaza: String,
