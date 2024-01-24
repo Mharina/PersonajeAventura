31e7d9f9233cc87d14e3b80d53ae1cf267db71b1
Marhina <marper387@alu.centroafuera.es>
2024-01-24 08:49:55 +0100

    falta articulo y mercader mas o menos

--- app/src/main/java/com/example/personaje/Mercader.kt
+++ app/src/main/java/com/example/personaje/Mercader.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.core.view.isInvisible
 
 class Mercader : AppCompatActivity() {
@@ -13,16 +14,27 @@ class Mercader : AppCompatActivity() {
         setContentView(R.layout.activity_mercader)
         val comerciar: Button = findViewById(R.id.button)
         val continuar: Button = findViewById(R.id.button2)
+        val arti: TextView = findViewById(R.id.art)
+        val compra: Button = findViewById(R.id.compra)
+        val venta: Button = findViewById(R.id.venta)
+        val cancela: Button = findViewById(R.id.cancelar)
+        val dbHelper = DatabaseHelper2(this)
+        val listArt : List<Articulo> = dbHelper.getArticulo()
+        val arrayArt = listArt.joinToString(separator = "\n") { it.toString() }
+
+        arti.text = "LISTA DE ARTICULOS\n"+arrayArt
+
         continuar.setOnClickListener{
             var intent = Intent(this@Mercader, Aventura::class.java)
             startActivity(intent)
         }
         comerciar.setOnClickListener{
-            //var intent = Intent(this@Mercader, Proximamente::class.java)
-            //startActivity(intent)
-            //comerciar.visibility(view.gone)
             comerciar.visibility = View.GONE
-            //comerciar.visibility = View.GONE
+            continuar.visibility = View.GONE
+            arti.visibility = View.VISIBLE
+            compra.visibility = View.VISIBLE
+            venta.visibility = View.VISIBLE
+            cancela.visibility = View.VISIBLE
         }
     }
 }
